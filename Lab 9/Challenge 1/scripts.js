526bf57c406bd558e5e933aa73e3c221e8c0f2d5
jacobvanz <90531886+jacobvanz@users.noreply.github.com>
2021-11-14 18:42:04 -0500

    lab9

--- Lab 9/Challenge 1/scripts.js
+++ Lab 9/Challenge 1/scripts.js
@@ -4,5 +4,5 @@ function stopTime() {
 var stop = new Date();
 var timePassed = stop.getTime() - start.getTime();
 var secondsPassed = timePassed / 600;
-alert(secondsPassed);
+alert("You have been on the page for: " + secondsPassed + " seconds");
 }
