9e4188f5bdc59f42df7c49a1fb2ab6c53d67ede4
jacobvanz <90531886+jacobvanz@users.noreply.github.com>
2021-11-14 19:18:11 -0500

    lab9

---
+++ Lab 9/Challenge 3/scripts.js
@@ -0,0 +1,6 @@
+function equals(){
+  var input1 = document.getElementById("input1").value;
+  var input2 = document.getElementById("input2").value;
+  var answer = Number(input1) + Number(input2);
+  document.getElementById("OUTPUT").innerHTML=answer
+}
